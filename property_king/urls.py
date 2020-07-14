da37d341ec28ca04325ca1944bb3e99ff78b4923
Gilwell <gilwellm@gmail.com>
2020-07-15 00:33:30 +0300

    System templating initialized

--- property_king/urls.py
+++ property_king/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
 
 urlpatterns = [
+    path('',include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
