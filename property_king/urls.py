3de21a3f746e69fb93e24630852909b55993acfd
Gilwell <gilwellm@gmail.com>
2021-04-22 23:31:42 +0300

    Property Manager App Template setting initialized

--- property_king/urls.py
+++ property_king/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('',include('accounts.urls')),
     path("dashboard/", include("dashboard.urls")),
+    path("properties/", include("properties.urls", namespace='properties')),
     path('admin/', admin.site.urls),
 ]
