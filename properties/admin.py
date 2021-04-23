80afc5f3e7f565f7ae2e8e2fd5c70f51b38831d3
Gilwell <gilwellm@gmail.com>
2021-04-23 12:43:44 +0300

    Properties and Propeties Model initialized

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Properties, PropertType
 
-# Register your models here.
+
+class PropertyTypeAdmin(admin.ModelAdmin):
+    list_display = ('name','created_date','updated_date')
+admin.site.register(PropertType, PropertyTypeAdmin)
+
+
+class PropertiesTypeAdmin(admin.ModelAdmin):
+    list_display = ('name','slug','email','notes','number_of_units','property_type','logo','city','state','contacts','created_date','updated_date')
+admin.site.register(Properties, PropertiesTypeAdmin)
--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class PropertType(models.Model):
+    name = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Properties(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    email = models.EmailField(max_length=75)
+    notes = models.TextField(max_length=2048*4)
+    number_of_units = models.CharField(max_length=255)
+    property_type = models.ForeignKey(PropertType, verbose_name=("Propert Type"), on_delete=models.CASCADE)
+    logo = models.ImageField(upload_to = "property_logos/")
+    city  =  models.CharField(max_length=255)
+    state  =  models.CharField(max_length=255)
+    contacts =  models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
