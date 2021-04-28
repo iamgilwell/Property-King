2da98941ecfc24cd9312ecb17efd6d54e8da6755
Gilwell <gilwellm@gmail.com>
2021-04-28 07:43:34 +0300

    Property Front-End - Listing Properties on the template

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from . models import Properties
 # Create your views here.
 
 def index(request):
-    return render(request,"properties/index.html")
+    properties = Properties.objects.all()
+    context  = {
+        'properties':properties
+    }
+    return render(request,"properties/index.html", context)
